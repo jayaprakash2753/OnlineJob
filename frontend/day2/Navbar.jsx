819f34842019fe32e9229714e3a7a95bc3011197
jayaprakash2753 <727821tucs101@skct.edu.in>
2024-03-19 10:30:43 +0530

    day 2

---
+++ frontend/day2/Navbar.jsx
@@ -0,0 +1,19 @@
+
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <>
+        <div className='nav'>
+            <div className='nave'>
+
+            <img src="C:\Users\praka\AppData\Local\Temp\cf845fce-1e88-4890-aefd-5cac5e0ef3c7_HatchfulExport-All.zip.3c7\logo.png"/></div>
+            <div className='nav1'>Jobs</div>
+            <div className='nav2'>Companies</div>
+            <div className='nav3'>About Us</div>
+            <div className='nav4'>Contact Us</div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
